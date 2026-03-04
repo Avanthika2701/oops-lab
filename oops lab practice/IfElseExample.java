a95b70ff539ea9bcf988f79e11e0f2e575b584c1
Avanthika2701 <ch.sc.u4cse25063@ch.students.amrita.edu>
2026-03-04 18:08:57 +0530

    Add files via upload

---
+++ oops lab practice/IfElseExample.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class ElseIfLadderExample
+{
+public static void main(String[] args)
+{
+Scanner sc= new Scanner (System.in);
+System.out.println("Enter the marks:");
+int marks= sc.nextInt();
+if (marks>=90)
+{
+System.out.println("Pass");
+}
+else
+{
+System.out.println("Fail");
+}
+}
+}
---
+++ oops lab practice/SwitchCaseExample.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class SwitchCaseExample
+{
+public static void main(String[] arg)
+{
+Scanner sc = new Scanner (System.in);
+System.out.println("Enter a number (1-7):5");
+int day = sc.nextInt();
+switch(day)
+{
+case 1 -> System.out.println("Monday");
+
+case 2 -> System.out.println("Tuesday");
+
+case 3 -> System.out.println("Wednesday");
+
+case 4 -> System.out.println("Thursday");
+
+case 5 -> System.out.println("Friday");
+
+case 6 -> System.out.println("Saturday");
+
+case 7 -> System.out.println("Sunday");
+
+default -> System.out.println("Invalid number.");
+
+}
+}
+}
